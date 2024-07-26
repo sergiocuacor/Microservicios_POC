d5dd69dbfcd154e62c3a4f60fcc4ae7adf616a09
sergiocc <2sergiocuadrado2000@gmail.com>
2024-07-26 11:28:33 +0200

    [ADD] eserver config

--- customer/src/main/java/com/sergiocuacor/microservices/customer/CustomerRegistrationRequest.java
+++ customer/src/main/java/com/sergiocuacor/microservices/customer/CustomerRegistrationRequest.java
@@ -3,5 +3,6 @@ package com.sergiocuacor.microservices.customer;
 public record CustomerRegistrationRequest(String firstName, String lastName, String email) {
 
 
+
 }
 // Utilizada para realizar las peticiones HTTP para la creacion nuevos customers
