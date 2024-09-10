8609a1c007bbeb457b391c4ce2aef026c48035b6
sergiocc <2sergiocuadrado2000@gmail.com>
2024-09-10 09:40:27 +0200

    rabbitmq config

---
+++ amqp/src/main/java/com/sergiocuacor/microservices/amqp/RabbitMQConfig.java
@@ -0,0 +1,44 @@
+package com.sergiocuacor.microservices.amqp;
+
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    // Provides a way to create connections to a messaging broker(rabbit). Similar to Connection in JDBC
+    private final ConnectionFactory connectionFactory;
+
+    public RabbitMQConfig(ConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory() {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jacksonConverter());
+        return factory;
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jacksonConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public MessageConverter jacksonConverter() { //Converter between Java objects and Json
+        MessageConverter jackson2MessageConverter = new Jackson2JsonMessageConverter();
+        return jackson2MessageConverter;
+    }
+}
